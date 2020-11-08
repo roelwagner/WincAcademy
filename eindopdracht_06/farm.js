7ea30cc662e933679a7068c234fdab7c95c42f76
Roel Wagner <rlwgnr@gmail.com>
2020-11-08 16:52:08 +0100

    get_yield_for_crop function operational

--- eindopdracht_06/farm.js
+++ eindopdracht_06/farm.js
@@ -2,6 +2,11 @@ const get_yield_for_plant = (crop) => {
     return crop.yield;
 }
 
+const get_yield_for_crop = (input) => {
+    return input.num_crops * get_yield_for_plant(input.crop);
+}
+
 module.exports = {
     get_yield_for_plant,
+    get_yield_for_crop,
 };
