c7ff8b256822f4a256597608e880cfb788570bae
Roel Wagner <rlwgnr@gmail.com>
2020-11-08 16:58:03 +0100

    get_total_yield function operational

--- eindopdracht_06/farm.js
+++ eindopdracht_06/farm.js
@@ -6,7 +6,14 @@ const get_yield_for_crop = (input) => {
     return input.num_crops * get_yield_for_plant(input.crop);
 }
 
+const get_total_yield = (allCrops) => {
+    return allCrops.crops.reduce((total, crops) => {
+        return total + get_yield_for_crop(crops);
+    },0)
+}
+
 module.exports = {
     get_yield_for_plant,
     get_yield_for_crop,
+    get_total_yield,
 };
