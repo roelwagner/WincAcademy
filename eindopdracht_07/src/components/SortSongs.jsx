49103bf82c47b4adf39335654b0e4e6ffb74076a
Roel Wagner <rlwgnr@gmail.com>
2020-12-20 13:58:09 +0100

    Added sorting component

---
+++ eindopdracht_07/src/components/SortSongs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SortSongs = () => {
+    return(
+        <option>
+            <select>Someting</select>
+        </option>
+    );
+}
+
+export default SortSongs;
