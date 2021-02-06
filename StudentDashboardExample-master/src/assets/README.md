3700457de3b7370bef7f60fa63255357164b0119
Roel Wagner <rlwgnr@gmail.com>
2021-02-06 18:02:53 +0100

    cleaned directory

--- StudentDashboardExample-master/src/assets/README.md
+++
@@ -1,1 +0,0 @@
-Assets like images etc
--- test/test.mjs
+++
@@ -1,9 +0,0 @@
-import {data} from "./data.mjs";
-
-const getAmountOfStudents = array => {
-    const students = array.map(element => element.map(item => item.active));
-    const activeStudents = students.flat().filter(item => item === true);
-    return activeStudents.length;
-}
-
-console.log(getAmountOfStudents(data))
